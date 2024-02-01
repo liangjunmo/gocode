deb760622c5c48e10c09e796a547ae341f4ab7ba
liangjunmo <liangjunmo@foxmail.com>
2024-02-02 00:17:58 +0800

    refactor tests

--- parse_test.go
+++ parse_test.go
@@ -8,48 +8,35 @@ import (
 )
 
 func TestParseErrorIsNil(t *testing.T) {
-	var err error
-	code := Parse(err)
-	require.Equal(t, SuccessCode, code)
+	require.Equal(t, SuccessCode, Parse(nil))
 }
 
 func TestParseErrorIsNotNilButIsNotCode(t *testing.T) {
-	err := fmt.Errorf("err")
-	code := Parse(err)
-	require.Equal(t, DefaultCode, code)
+	require.Equal(t, DefaultCode, Parse(fmt.Errorf("err")))
 }
 
 func TestParseErrorIsWrappedButIsNotCode(t *testing.T) {
-	err := fmt.Errorf("err")
-	err = fmt.Errorf("%v: %w", err, fmt.Errorf("err"))
-
-	code := Parse(err)
-	require.Equal(t, DefaultCode, code)
+	err := fmt.Errorf("%v: %w", fmt.Errorf("err"), fmt.Errorf("err"))
+	require.Equal(t, DefaultCode, Parse(err))
 }
 
 func TestParseErrorIsCode(t *testing.T) {
 	notFoundCode := Code("NotFound")
 	err := fmt.Errorf("%w", notFoundCode)
-	code := Parse(err)
-	require.Equal(t, notFoundCode, code)
+	require.Equal(t, notFoundCode, Parse(err))
 }
 
 func TestParseErrorIsWrappedCode(t *testing.T) {
 	notFoundCode := Code("NotFound")
-
-	err := fmt.Errorf("err")
-	err = fmt.Errorf("%v: %w", err, notFoundCode)
-
-	code := Parse(err)
-	require.Equal(t, notFoundCode, code)
+	err := fmt.Errorf("%v: %w", fmt.Errorf("err"), notFoundCode)
+	require.Equal(t, notFoundCode, Parse(err))
 }
 
 func TestParseErrorIsMultipleWrappedCode(t *testing.T) {
 	invalidRequestCode := Code("invalidRequest")
 	notFoundCode := Code("NotFound")
 
-	err := fmt.Errorf("err")
-	err = fmt.Errorf("%v: %w", err, invalidRequestCode)
+	err := fmt.Errorf("%v: %w", fmt.Errorf("err"), invalidRequestCode)
 
 	for i := 0; i < 10; i++ {
 		err = fmt.Errorf("%v: %w", err, Code(fmt.Sprintf("%d", i)))
